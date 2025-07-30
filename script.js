168879e0a9665ff658ee0079195ccf241b604d05
Vendy A15 <andyvenson99@gmail.com>
2025-07-30 00:14:47 -0400

    J'ai créé les input avec succès. Il manque seulement les labels. Je vais maintenant essayer une autrz approche.

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var compteurInterest = 0;
 const downloadButton = document.getElementById("download-button");
 
 let inputList = document.querySelectorAll("input");
-inputList.forEach((input) => {
+inputList.forEach(input => {
   input.addEventListener("change", () => {
     verifyAndChange(firstNameInput, firstName, "firstName");
     verifyAndChange(lastNameInput, lastName, "lastName");
@@ -40,7 +40,7 @@ inputList.forEach((input) => {
 });
 
 let textAreaList = document.querySelectorAll("textarea");
-textAreaList.forEach((textArea) => {
+textAreaList.forEach(textArea => {
   textArea.addEventListener("change", () => {
     verifyAndChange(
       interestElementsInput,
@@ -93,16 +93,41 @@ newInterest.addEventListener("click", () => {
   interestContainer.appendChild(copiedInterest);
 
   let interestInputContainer = document.querySelector(
-    ".interest-input-contaiiner"
+    ".interest-input-container"
   );
-  let echantillonInput = document.compteurInterest++;
+  let echantillonInput = document.querySelector(".interests-input");
+  let echantillonThemeInput = document.getElementById("interest-theme-input");
+  let echantillonElementsInput = document.getElementById(
+    "interest-elements-input"
+  );
+
+  let copiedInput = echantillon.cloneNode(false);
+  copiedInput.id = "interests-input" + compteurInterest;
+  let copiedThemeInput = echantillonThemeInput.cloneNode(true);
+  copiedThemeInput.id = "interest-theme-input" + compteurInterest;
+  copiedThemeInput.value = "";
+
+  let copiedElementsInput = echantillonElementsInput.cloneNode(true);
+  copiedElementsInput.id = "interest-elements-input" + compteurInterest;
+  copiedElementsInput.value = "";
+  
+  let fieldset = document.createElement("fieldset");
+
+  echantillonInput.appendChild(fieldset);
+  fieldset.appendChild(copiedThemeInput);
+  fieldset.appendChild(copiedElementsInput);
+  interestInputContainer.appendChild(echantillonInput);
+
+  compteurInterest++;
   console.log(compteurInterest);
+  console.log(copiedElementsInput.id);
+  console.log(copiedThemeInput.id);
 });
 
 downloadButton.addEventListener("click", () => {
   const captureZone = document.querySelector(".curiculum");
   html2canvas(captureZone)
-    .then((canvas) => {
+    .then(canvas => {
       // Convertit le canvas en une image PNG
       const imageURL = canvas.toDataURL("image/png");
       // Crée un lien de téléchargement
@@ -111,7 +136,7 @@ downloadButton.addEventListener("click", () => {
       a.download = "pixel-art" + ".png";
       a.click();
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Erreur lors de la capture :", error);
       alert("Une erreur est survenue lors de la capture !");
     });
